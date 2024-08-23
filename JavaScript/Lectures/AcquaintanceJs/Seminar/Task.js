8e43b329be47efa5408034bb6560d9ff9f36d290
Savenok <romasav2017@gmail.com>
2024-08-23 11:52:37 +0300

    task1

---
+++ JavaScript/Lectures/AcquaintanceJs/Seminar/Task.js
@@ -0,0 +1,15 @@
+console.log("Задача 1!!")
+
+var temperatureC = 25.5
+console.log(`Температура в Цельсия: ${temperatureC}`)
+
+var temperatureF = ( 9 /5 ) * temperatureC + 32
+console.log(`Температура в Фаренгейта: ${temperatureF}`)
+
+console.log("Задача 2!")
+
+let name = "Роман"
+
+let admin = name
+console.log(`Мое имя: ${name},
+    Имя админа: ${admin}!`)
