614617f2679c2ce3b47140b475c530d68d035cdb
Savenok <Romasav2017@gmail.com>
2024-10-01 23:42:35 +0300

    01.10.2024

---
+++ JavaScript/Lectures/UseJson/homework/data.js
@@ -0,0 +1,28 @@
+const dataInfo = `
+[
+    {
+        "number": "112",
+        "language": "en",
+        "insult": "If I wanted to kill myself, I would climb to the number of your chromosomes and then jump to your IQ.",
+        "scr": "https://play-lh.googleusercontent.com/6UgEjh8Xuts4nwdWzTnWH8QtLuHqRMUB7dp24JYVE2xcYzq4HA8hFfcAbU-R-PC_9uA1"
+        },
+    {
+        "number": "235",
+        "language": "en",
+        "insult": "I guess those penis enlargement pills are working, you're twice the dick you were yesterday.",
+        "scr": "https://play-lh.googleusercontent.com/6UgEjh8Xuts4nwdWzTnWH8QtLuHqRMUB7dp24JYVE2xcYzq4HA8hFfcAbU-R-PC_9uA1"
+    },
+    {
+        "number": "1948",
+        "language": "en",
+        "insult": "The best part of you ran down the crack of your mom's ass.",
+        "scr": "https://play-lh.googleusercontent.com/6UgEjh8Xuts4nwdWzTnWH8QtLuHqRMUB7dp24JYVE2xcYzq4HA8hFfcAbU-R-PC_9uA1"
+    },
+    {
+        "number": "152",
+        "language": "en",
+        "insult": "Here's 20 cents, call all your friends and give me back the change.",
+        "scr": "https://play-lh.googleusercontent.com/6UgEjh8Xuts4nwdWzTnWH8QtLuHqRMUB7dp24JYVE2xcYzq4HA8hFfcAbU-R-PC_9uA1"
+    }
+]
+`
---
+++ JavaScript/Lectures/UseJson/seminar/data.js
@@ -0,0 +1,33 @@
+const dataInfo = `
+[
+    {
+        "id": 1,
+        "name": "Fedor",
+        "secondName": "Pupkin",
+        "age": 16
+    },
+    {
+        "id": 2,
+        "name": "Max",
+        "secondName": "Vovki",
+        "age": 13
+    },
+    {
+        "id": 3,
+        "name": "Kate",
+        "secondName": "Luntik",
+        "age": 12
+    },
+    {
+        "id": 4,
+        "name": "Igor",
+        "secondName": "Xiro",
+        "age": 11
+    },
+    {
+        "id": 5,
+        "name": "Masha",
+        "secondName": "Sovka",
+        "age": 17
+    }
+]`
